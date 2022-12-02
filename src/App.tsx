fadda33be2df2a776a9e6caec88ed0924a0d6e10
karpeitaemukan <l07wkdgustn07l@gmail.com>
2022-12-02 17:36:11 +0900

    header setting part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 
 function App() {
   return (
-    <div>
-    
-    </div>
+    <>
+<BrowserRouter>
+<Header /> 
+<Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/tv" element={<Tv/>}/>      
+<Route path="/search" element={<Search/>}/> 
+</Routes>
+</BrowserRouter>
+    </>
   );
 }
 
