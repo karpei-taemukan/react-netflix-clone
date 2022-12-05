6d6f4781924c5797256629b11d8d7452f78173c1
karpeitaemukan <l07wkdgustn07l@gmail.com>
2022-12-05 17:54:52 +0900

    fix slider bug

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -61,6 +61,12 @@ height: 200px;
 color:red;
 background-size: cover;
 background-position: center center;
+&:first-child{
+    transform-origin: center left;
+};
+&:last-child{
+    transform-origin: center right;
+};
 `;
 
 const rowVars = {
@@ -75,6 +81,70 @@ const rowVars = {
     },
 }
 
+//--------------------------------------------------------------------
+
+const boxVars = {
+    normal:{
+        scale: 1,
+    },
+    hover:{
+        scale: 1.3,
+        y:-50,
+        transition:{
+            duration: 0.3,
+            delay: 0.3,
+            type:"tween"
+        }
+    }
+}
+
+
+//--------------------------------------------------------------------
+
+
+const Info = styled(motion.div)`
+padding: 10px;
+background-color: ${(props) => props.theme.black.lighter};
+opacity: 0;
+position: absolute;
+width: 100%;
+bottom:0;
+h4{
+    text-align: center;
+    font-size: 8px;
+    color: white;
+}
+`;
+
+const infoVars = {
+    hover:{
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duaration: 0.3,
+            type: "tween",
+          },
+    },
+}
+
+const Thumb = styled(motion.div)`
+display: flex;
+justify-content: space-around;
+align-items: center;
+`;
+
+const Like = styled(motion.svg)`
+width: 30px;
+height: 30px;
+`
+
+const Dislike = styled(motion.svg)`
+width: 30px;
+height: 30px;
+`
+//--------------------------------------------------------------------
+
+
 function Home(){
     const {data, isLoading} = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"], 
@@ -85,6 +155,8 @@ function Home(){
 
 const [index,setIndex] = useState(0);
 const [leaving, setLeaving] = useState(false);
+//const [likeThumb, setLikethumb] = useState(false);
+///const [dislikeThumb, setDislikethumb] = useState(false);
 
 //--------------------------------------------------------------------
 const increaseIndex = () => {
@@ -98,8 +170,14 @@ const increaseIndex = () => {
 };
 const toggleLeaving = () => {
     setLeaving(current => !current);
-}
+};
 
+/*const toggleLikethumb = () => {
+    setLikethumb(current => !current);
+}
+const toggleDislikethumb = () => {
+    setDislikethumb(current => !current);
+}*/
 //--------------------------------------------------------------------
 const offset = 6;
 
@@ -110,9 +188,10 @@ const offset = 6;
         <Loader>Loading...</Loader> 
         :
         <>
-        <Banner bgphoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+        <Banner
+        bgphoto={makeImagePath(data?.results[0].backdrop_path || "")}>
            {/* || ""(fallback) 쓴 이유는 
-           data가 정의 되지않거나 api에서 data가 안옴 */}
+           data가 정의 되지않거나 api에서 data가 안옴 방지 */}
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
         </Banner>
@@ -130,17 +209,55 @@ const offset = 6;
             animate="visible"
             exit="exit"
             transition={{type:"tween", duration: 1}}
-             key={index}>
+            key={index}>
             {data?.results.slice(1)
             .slice(offset*index, offset*index+offset)
             .map((movie)=>
             <Box 
+            variants={boxVars}
+            initial="normal"
             key={movie.id}
+            whileHover="hover"
+            transition={{type: "tween"}}
             bgphoto={makeImagePath(movie.backdrop_path, "w500")}
-            />)}
+            >
+    
+            <Info variants={infoVars}><h4>{movie.title}</h4>
+            {/*<Thumb>
+            <Like
+            onClick={toggleLikethumb}
+            style={{fill: likeThumb ? "blue" : "black" }}
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 512 512">
+            <path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 25.3-19.5 46-44.3 47.9c7.7 8.5 12.3 19.8 12.3 32.1c0 23.4-16.8 42.9-38.9 47.1c4.4 7.2 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/>
+            </Like>
+            <Dislike 
+            onClick={toggleDislikethumb}
+            style={{fill: dislikeThumb ? "blue" : "black" }}
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 512 512">
+            <path d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2H464c26.5 0 48-21.5 48-48c0-25.3-19.5-46-44.3-47.9c7.7-8.5 12.3-19.8 12.3-32.1c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48H294.5c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7V192v48 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 320H96c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32H32C14.3 32 0 46.3 0 64V288c0 17.7 14.3 32 32 32z"/>
+            </Dislike>
+            </Thumb>*/}
+            </Info>
+            
+            </Box>
+            
+            )}
+            
+            {/*  $$$
+            부모컴포넌트에 variants가 있다면 자식 컴포넌트에도 variants가 상속된다 
+            즉, 
+           <Box variants={boxVars}  whileHover="hover">
+           <Info  whileHover="hover"/>
+           </Box>
+                $$$
+            */}
+
             </Row>
             </AnimatePresence>
         </Slider>
+        
 </Wrapper>
 )}
 
